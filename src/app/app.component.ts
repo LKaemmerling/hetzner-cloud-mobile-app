e83f64c61b84ac3526ec9d2a4180f7135c5279fa
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-01-30 13:23:38 +0100

    Initial Build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import {Component, ViewChild} from '@angular/core';
-import {Nav, NavController, Platform} from 'ionic-angular';
+import {Nav, Platform} from 'ionic-angular';
 import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
 
-import {TabsPage} from '../pages/tabs/tabs';
 import {ProjectsService} from "../models/project/ProjectsService";
 import {HomePage} from "../pages/home/home";
 import {ProjectsPage} from "../pages/projects/projects";
 import {AboutPage} from "../pages/about/about";
 import {ServersPage} from "../pages/servers/servers";
+import {Storage} from "@ionic/storage";
 
 @Component({
   templateUrl: 'app.html'
@@ -17,22 +17,28 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
   rootPage: any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public projects: ProjectsService) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public projects: ProjectsService, storage: Storage) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-      projects.loadProjects();
+      storage.ready().then(() => {
+        statusBar.styleDefault();
+
+        projects.loadProjects();
+        splashScreen.hide();
+      });
+
     });
   }
 
   openProjectsPage() {
     this.nav.setRoot(ProjectsPage);
   }
+
   openAboutPage() {
     this.nav.setRoot(AboutPage);
   }
+
   openServersPage() {
     this.nav.setRoot(ServersPage);
   }
--- src/models/project/project.ts
+++ src/models/project/project.ts
@@ -1,12 +1,9 @@
 import {Injectable} from "@angular/core";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {RestProvider} from "../../providers/rest/rest";
 
 @Injectable()
 export class project {
   public name: string;
   public api_key: string;
-  public servers: Array<any>;
 
   constructor(name: string, api_key: string) {
     this.name = name;
