c31bb30d73e4fec2f965ee471b35ad88bb515819
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-02-01 11:36:37 +0100

    Work around for #3 Get all Servers initial and filter the name locally.

--- src/pages/server/serverList/servers.ts
+++ src/pages/server/serverList/servers.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {ModalController, NavController} from 'ionic-angular';
+import {LoadingController, ModalController, NavController} from 'ionic-angular';
 import {ProjectsService} from "../../../models/project/ProjectsService";
 import {ServerPage} from "../server";
 import {addServerModal} from "../addServer/addServer";
@@ -14,12 +14,12 @@ export class ServersPage {
   public servers;
   public _search = false;
 
-  constructor(public navCtrl: NavController, public project: ProjectsService, public serverApiProvider: ServerApiProvider, public modal: ModalController) {
+  constructor(public navCtrl: NavController, public project: ProjectsService, public serverApiProvider: ServerApiProvider, public modal: ModalController, public loadingCtrl: LoadingController) {
     this.loadServers();
   }
 
-  public loadServers(searchTerm = null) {
-    this.serverApiProvider.getServers(searchTerm).then((data) => {
+  public loadServers() {
+    this.serverApiProvider.getServers().then((data) => {
       this.servers = data['servers'];
     });
   }
@@ -36,7 +36,12 @@ export class ServersPage {
 
   public delete(server) {
     if (confirm('Möchten Sie den Server ' + server.name + ' wirklich unwiederuflich löschen?')) {
-      this.serverApiProvider.delete(server.id).then((data) => this.loadServers());
+      var loader = this.loadingCtrl.create();
+      loader.present();
+      this.serverApiProvider.delete(server.id).then((data) => {
+        this.loadServers()
+        loader.dismiss();
+      });
     }
   }
 
@@ -44,8 +49,18 @@ export class ServersPage {
     this._search = true;
     // Reset items back to all of the items
 
-    this.loadServers(ev.target.value);
+// set val to the value of the ev target
+    var val = ev.target.value;
 
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.servers = this.servers.filter((item) => {
+        if (item == null) {
+          return false;
+        }
+        return (item.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 
   openCreateServerModal() {
