27492ba269ca7f4fc688929684a1797a1083df04
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-02-08 11:36:24 +0100

    Fix #22 & Close #18

--- src/models/project/project.ts
+++ src/models/project/project.ts
@@ -5,7 +5,7 @@ export class project {
   public name: string;
   public api_key: string;
 
-  constructor(name: string, api_key: string) {
+  constructor(name: string = '', api_key: string = '') {
     this.name = name;
     this.api_key = api_key;
   }
