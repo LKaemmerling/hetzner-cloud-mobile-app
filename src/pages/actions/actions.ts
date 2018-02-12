b817b17e8a9dc989cb0587088d34956a5c83f34b
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-02-12 14:59:13 +0100

    Generate Actions Page

---
+++ src/pages/actions/actions.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the ActionsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-actions',
+  templateUrl: 'actions.html',
+})
+export class ActionsPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ActionsPage');
+  }
+
+}
