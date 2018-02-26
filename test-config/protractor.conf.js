8d09aa0382ebbde885986854e04743b9ada8ff39
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-02-27 00:29:19 +0100

    Cleanup

--- test-config/protractor.conf.js
+++ test-config/protractor.conf.js
@@ -16,7 +16,7 @@ exports.config = {
     }
   },
   directConnect: true,
-  baseUrl: 'http://localhost:8100/',
+  baseUrl: 'http://localhost:8001/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
