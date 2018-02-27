9a70d5308ff202f870c84afac44367d291f0246a
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-02-27 01:30:30 +0100

    Implement Tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import {Page} from './app.po';
+import {by, element} from "protractor";
 
 describe('App', () => {
   let page: Page;
@@ -16,6 +17,18 @@ describe('App', () => {
       page.getTitle().then(title => {
         expect(title).toEqual('Hetzner Cloud Mobile');
       });
+    });
+    it('it should have a button on the start page if there are no projects', () => {
+      expect(element(by.buttonText('Jetzt erstes Projekt hinzufügen!')).isDisplayed()).toBe(true);
+    });
+
+
+    it('should has the right menu structure', () => {
+      element(by.xpath('//*[@id="nav"]/page-home[1]/ion-header[1]/ion-navbar[1]/button[2]')).click();
+      expect(element(by.buttonText('Meine Projekte')).getCssValue('display')).toEqual('flex');
+      expect(element(by.buttonText('Hetzner Status')).getCssValue('display')).toEqual('flex');
+      expect(element(by.buttonText('Einstellungen')).getCssValue('display')).toEqual('flex');
+
     });
   });
 });
