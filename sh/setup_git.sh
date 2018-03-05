532dae26becae793cfcb359443068d41889716f5
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-03-05 13:28:06 +0100

    Add Git Setup Script

---
+++ sh/setup_git.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Remove default Origin";
+git remote remove origin
+echo "Set default Origin to Gitlab";
+git remote add origin git@gitlab.com:LKDevelopment/hetzner-cloud-mobile-app.git
+echo "Gitlab Mirror";
+git remote set-url --add --push origin git@gitlab.com:LKDevelopment/hetzner-cloud-mobile-app.git
+echo "GitHub Mirror";
+git remote set-url --add --push origin git@github.com:LKDevelopment/hetzner-cloud-mobile-app.git
+echo "Ionic JS Mirror";
+git remote set-url --add --push origin git@git.ionicjs.com:lkdev/hetzner-cloud-app.git
+echo "Setup done";
