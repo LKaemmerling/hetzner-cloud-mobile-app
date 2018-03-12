150fdd687be069a4fdcca79b86488223be6826f9
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-03-12 17:18:21 +0100

    Remove novnc

--- src/pages/server/console/console.ts
+++ src/pages/server/console/console.ts
@@ -3,6 +3,7 @@ import {LoadingController, NavController, NavParams, ViewController} from "ionic
 import {ServerApiProvider} from "../../../providers/server-api/server-api";
 import {DomSanitizer} from "@angular/platform-browser";
 
+import RFB from '@novnc/novnc/core/rfb.js';
 
 @Component({
   selector: 'modal-console',
@@ -14,17 +15,17 @@ export class consoleModal {
   public wss_url: any = null;
   public vnc_url: string = null;
   public payload;
-
+  public rfb;
   constructor(public viewCtrl: ViewController, public serverApiProvider: ServerApiProvider, public navParams: NavParams, public navCtrl: NavController, public loadingCtrl: LoadingController, public santizer: DomSanitizer) {
     const parseUrl = require('parse-url');
     this.server = navParams.get('server');
     this.serverApiProvider.requestConsole(this.server.id).then((response) => {
       this.wss_url = parseUrl(response['wss_url']);
-      console.log(this.wss_url);
+    this.rfb = new RFB(document.getElementById('console_container'),response['wss_url'],{ credentials: { password: response['password'] }});
       this.password = response['password'];
-      this.vnc_url = 'assets/novnc/vnc.html?host=' + this.wss_url.resource + '&password=' + this.password + "&autoconnect=true&encrypt=true&resize=remote&server_id=" + this.wss_url.query.server_id + "&token=" + this.wss_url.query.token;
+     // this.vnc_url = 'assets/novnc/vnc.html?host=' + this.wss_url.resource + '&password=' + this.password + "&autoconnect=true&encrypt=true&resize=remote&server_id=" + this.wss_url.query.server_id + "&token=" + this.wss_url.query.token;
       //inAppBrowser.create(this.vnc_url,'_blank').show();
-      this.payload = santizer.bypassSecurityTrustHtml('<iframe src="' + this.vnc_url + '" scrolling="no" frameborder="0"></iframe>')
+
     });
   }
 
