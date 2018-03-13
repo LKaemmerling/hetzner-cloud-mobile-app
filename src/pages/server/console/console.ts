9b940f2a2635fb7ac85c202cd3632923e891bed6
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-03-13 07:10:19 +0100

    Working von VNC Console

--- src/pages/server/console/console.ts
+++ src/pages/server/console/console.ts
@@ -11,23 +11,14 @@ import RFB from '@novnc/novnc/core/rfb.js';
 })
 export class consoleModal {
   public server: any;
-  public password: string = null;
-  public wss_url: any = null;
-  public vnc_url: string = null;
   public payload;
   public rfb;
 
   constructor(public viewCtrl: ViewController, public serverApiProvider: ServerApiProvider, public navParams: NavParams, public navCtrl: NavController, public loadingCtrl: LoadingController, public santizer: DomSanitizer) {
-    const parseUrl = require('parse-url');
     this.server = navParams.get('server');
     this.serverApiProvider.requestConsole(this.server.id).then((response) => {
-      this.wss_url = parseUrl(response['wss_url']);
       this.rfb = new RFB(document.getElementById('console_container'), response['wss_url'], {credentials: {password: response['password']}});
-      RFB.focus();
-      this.password = response['password'];
-      // this.vnc_url = 'assets/novnc/vnc.html?host=' + this.wss_url.resource + '&password=' + this.password + "&autoconnect=true&encrypt=true&resize=remote&server_id=" + this.wss_url.query.server_id + "&token=" + this.wss_url.query.token;
-      //inAppBrowser.create(this.vnc_url,'_blank').show();
-
+      this.rfb.focus();
     });
   }
 
