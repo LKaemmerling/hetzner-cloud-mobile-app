5d9f361bd3edce904ee7d4fc68b0ddfa9e8a4602
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-03-13 07:28:38 +0100

    Working von VNC Console

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -52,6 +52,7 @@ import {shareProjectModal} from "../pages/projects/shareProject/shareProject";
 import {NgxQRCodeModule} from "@lkdevelopment/ngx-qrcode/dist";
 import {ComponentsModule} from "../components/components.module";
 import {consoleModal} from "../pages/server/console/console";
+import {Keyboard} from "@ionic-native/keyboard";
 
 const IonicPro = Pro.init('359b3ec5', {
   appVersion: "1.2.0"
@@ -181,7 +182,8 @@ export class MyErrorHandler implements ErrorHandler {
     InAppBrowser,
     AppVersion,
     FingerprintAIO,
-    BarcodeScanner
+    BarcodeScanner,
+    Keyboard
   ]
 })
 export class AppModule {
--- src/pages/server/console/console.ts
+++ src/pages/server/console/console.ts
@@ -4,6 +4,7 @@ import {ServerApiProvider} from "../../../providers/server-api/server-api";
 import {DomSanitizer} from "@angular/platform-browser";
 
 import RFB from '@novnc/novnc/core/rfb.js';
+import {Keyboard} from "@ionic-native/keyboard";
 
 @Component({
   selector: 'modal-console',
@@ -14,10 +15,11 @@ export class consoleModal {
   public payload;
   public rfb;
 
-  constructor(public viewCtrl: ViewController, public serverApiProvider: ServerApiProvider, public navParams: NavParams, public navCtrl: NavController, public loadingCtrl: LoadingController, public santizer: DomSanitizer) {
+  constructor(public viewCtrl: ViewController, public serverApiProvider: ServerApiProvider, public navParams: NavParams, public navCtrl: NavController, public loadingCtrl: LoadingController, public keyboard: Keyboard) {
     this.server = navParams.get('server');
     this.serverApiProvider.requestConsole(this.server.id).then((response) => {
       this.rfb = new RFB(document.getElementById('console_container'), response['wss_url'], {credentials: {password: response['password']}});
+      this.keyboard.show();
       this.rfb.focus();
     });
   }
