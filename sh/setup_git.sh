350cb9b1e9f698003e247915456c6033a47b4d7c
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-03-14 16:13:38 +0100

    Add Notice about Umfrage

--- sh/setup_git.sh
+++ sh/setup_git.sh
@@ -7,8 +7,6 @@ echo "Gitlab Mirror";
 git remote set-url --add --push origin git@gitlab.com:LKDevelopment/hetzner-cloud-mobile-app.git
 echo "GitHub Mirror";
 git remote set-url --add --push origin git@github.com:LKDevelopment/hetzner-cloud-mobile-app.git
-echo "Ionic JS Mirror";
-git remote set-url --add --push origin git@git.ionicjs.com:lkdev/hetzner-cloud-app.git
 echo "Setup done now fetch everything from gitlab";
 git fetch
 git push --
