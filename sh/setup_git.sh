e68c79e29d75cc24591a6b16855723b0e4f5a245
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-03-22 17:02:16 +0100

    Use gitlabs Mirror function

--- sh/setup_git.sh
+++ sh/setup_git.sh
@@ -5,8 +5,6 @@ echo "Set default Origin to Gitlab";
 git remote add origin git@gitlab.com:LKDevelopment/hetzner-cloud-mobile-app.git
 echo "Gitlab Mirror";
 git remote set-url --add --push origin git@gitlab.com:LKDevelopment/hetzner-cloud-mobile-app.git
-echo "GitHub Mirror";
-git remote set-url --add --push origin git@github.com:LKDevelopment/hetzner-cloud-mobile-app.git
 echo "Setup done now fetch everything from gitlab";
 git fetch
 git push --set-upstream origin master
