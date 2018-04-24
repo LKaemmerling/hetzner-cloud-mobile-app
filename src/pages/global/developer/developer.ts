90d841ac774a39580259495ecc64e85483221ee4
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-04-24 13:36:01 +0200

    Working on Robot

--- src/pages/global/developer/developer.ts
+++ src/pages/global/developer/developer.ts
@@ -1,6 +1,9 @@
 import {Component} from '@angular/core';
 import {Device} from "@ionic-native/device";
 import {ConfigService} from "../../../modules/hetzner-app/config/config.service";
+import {Storage} from "@ionic/storage";
+import {NavController} from "ionic-angular";
+import {SettingsPage} from "../settings/settings";
 
 
 /**
@@ -19,16 +22,23 @@ export class DeveloperPage {
    * @param {Device} device
    * @param {ConfigService} config
    */
-  constructor(protected device: Device, protected config: ConfigService) {
+  constructor(protected device: Device, protected config: ConfigService, protected storage: Storage, protected nav: NavController) {
     this.feature_flags = this.config.getFeatureFlag();
   }
 
   toggleFlag(name: string) {
-    if(this.feature_flags[name] == false){
+    if (this.feature_flags[name] == false) {
       alert('This feature could be unstable! Use it on your own risk!')
     }
     this.config.setFeatureFlag(name, !this.feature_flags[name]);
     this.feature_flags = this.config.getFeatureFlag();
+  }
+
+  removeDeveloperMode() {
+    this.storage.set('developer_mode', false);
+    this.config.developer_mode = false;
+    this.config.setFeatureFlag('robot', false);
+    this.nav.setRoot(SettingsPage);
   }
 
 }
