33d189e589e5f1d9563116d5a8400e1752f46eac
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-05-03 17:19:57 +0200

    Add Server Market

--- src/pages/global/home/home.ts
+++ src/pages/global/home/home.ts
@@ -1,37 +1,45 @@
-import { Component } from '@angular/core';
-import { ModalController, NavController } from 'ionic-angular';
-import { ProjectsService } from '../../../modules/hetzner-cloud-data/project/projects.service';
-import { ProjectsPage } from '../../cloud/projects/projects';
-import { addProjectModal } from '../../cloud/projects/addProject/addProject';
+import {Component} from '@angular/core';
+import {ModalController, NavController} from 'ionic-angular';
+import {ProjectsService} from '../../../modules/hetzner-cloud-data/project/projects.service';
+import {ProjectsPage} from '../../cloud/projects/projects';
+import {addProjectModal} from '../../cloud/projects/addProject/addProject';
+import {SplashScreen} from "@ionic-native/splash-screen";
 
 /**
  * This is the basic start screen
  */
 @Component({
-    selector: 'page-home',
-    templateUrl: 'home.html',
+  selector: 'page-home',
+  templateUrl: 'home.html',
 })
 export class HomePage {
-    /**
-     * Constructor
-     * @param {NavController} navCtrl
-     * @param {ProjectsService} project
-     * @param {ModalController} modal
-     */
-    constructor(
-        protected navCtrl: NavController,
-        protected project: ProjectsService,
-        protected modal: ModalController
-    ) {}
+  /**
+   * Constructor
+   * @param {NavController} navCtrl
+   * @param {ProjectsService} project
+   * @param {ModalController} modal
+   */
+  constructor(
+    protected navCtrl: NavController,
+    protected project: ProjectsService,
+    protected modal: ModalController,
+    protected splashScreen: SplashScreen
+  ) {
+  }
 
-    /**
-     * This open the create project modal
-     */
-    public openCreateProject() {
-        let modal = this.modal.create(addProjectModal);
-        modal.onDidDismiss(() => {
-            this.navCtrl.setRoot(ProjectsPage);
-        });
-        modal.present();
-    }
+  public ionViewDidLoad() {
+    this.splashScreen.hide();
+  }
+
+
+  /**
+   * This open the create project modal
+   */
+  public openCreateProject() {
+    let modal = this.modal.create(addProjectModal);
+    modal.onDidDismiss(() => {
+      this.navCtrl.setRoot(ProjectsPage);
+    });
+    modal.present();
+  }
 }
